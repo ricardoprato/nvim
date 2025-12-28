eb078fa4f760d7e1927669e2c34b0923c7d6dd55
Ricardo Prato <ricardoprato36@gmail.com>
2025-12-28 11:04:24 -0300

    del: unnused conf

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -10,27 +10,3 @@ if odoo.is_odoo_project() then
   odoo.setup_odoo_buffer()
   odoo.optimize_for_large_codebase()
 end
-
--- Python-specific settings ===================================================
-
--- Set textwidth for Black formatter (default is 88)
-vim.opt_local.textwidth = 88
-vim.opt_local.colorcolumn = '+1'
-
--- Folding for Python
-vim.opt_local.foldmethod = 'indent'
-vim.opt_local.foldlevel = 2
-
--- Indentation (following PEP 8)
-vim.opt_local.tabstop = 4
-vim.opt_local.shiftwidth = 4
-vim.opt_local.softtabstop = 4
-vim.opt_local.expandtab = true
-
--- DAP keybindings hint (only show once per session)
-if vim.fn.executable('debugpy') == 1 and not vim.g.dap_hint_shown then
-  vim.defer_fn(function()
-    vim.notify('DAP: F5=Continue, F9=Breakpoint, F10=StepOver, F11=StepInto', vim.log.levels.INFO)
-    vim.g.dap_hint_shown = true
-  end, 1000)
-end
